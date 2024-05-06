89e19e679836d7fd063e17ba7551252235965985
praneeth-katuri <praneethkaturi@outlook.com>
2024-05-06 17:04:44 +0530

    feat: added solution for 013 lcm and hcf

---
+++ 013_lcm_and_hcf.py
@@ -0,0 +1,12 @@
+def gcd(a, b):
+    if b == 0:
+        return a
+    return gcd(b, a % b)
+
+def lcm(a, b):
+    return (a*b)//gcd(a, b)
+
+t = int(input())
+for _ in range(t):
+    a, b = map(int, input().split())
+    print(lcm(a,b), gcd(a,b))
