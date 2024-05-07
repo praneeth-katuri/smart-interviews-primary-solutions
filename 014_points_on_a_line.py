4fd31190ac141c954cb4cc3b666fe518b4086a6a
praneeth-katuri <praneethkaturi@outlook.com>
2024-05-07 09:44:12 +0530

    feat: added solution for 014 points on a line

---
+++ 014_points_on_a_line.py
@@ -0,0 +1,8 @@
+t = int(input())
+for _ in range(t):
+    x1, y1 = map(int, input().split())
+    x2, y2 = map(int, input().split())
+    x3, y3 = map(int, input().split())
+
+    area = x1 * (y2 - y3) + x2 * (y3 - y1) + x3 * (y1 - y2)
+    print('No' if area else 'Yes')
